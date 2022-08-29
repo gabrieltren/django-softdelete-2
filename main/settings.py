a69b38af2e5c24e75213aa942c27348866a17bf4
Gabrieltren <gabriel.silveira1207@gmail.com>
2022-08-29 18:16:43 -0300

    heroku!

--- main/settings.py
+++ main/settings.py
@@ -10,6 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import django_on_heroku
+django_on_heroku.settings(locals())
+
+
+
 from pathlib import Path
 from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
